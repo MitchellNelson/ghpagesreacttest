c2d0fee138883f47e773402ab3dcf57c846b41fa
Mitchell Nelson <nels4929@stthomas.edu>
2020-11-09 20:43:01 -0600

    Updates

---
+++ precache-manifest.48d46b5228c7e0ecc08275463d9db87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0ca60bd63613ae9e414032052e91a1",
+    "url": "/ghpagesreacttest/index.html"
+  },
+  {
+    "revision": "3a5c5f8c0a855d48f431",
+    "url": "/ghpagesreacttest/static/css/main.8967ed85.chunk.css"
+  },
+  {
+    "revision": "ce4ccbf9647d44913871",
+    "url": "/ghpagesreacttest/static/js/2.abb4a40d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ghpagesreacttest/static/js/2.abb4a40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5c5f8c0a855d48f431",
+    "url": "/ghpagesreacttest/static/js/main.86ae3d54.chunk.js"
+  },
+  {
+    "revision": "43230c2e98c33c17a71e",
+    "url": "/ghpagesreacttest/static/js/runtime-main.c0f5f2b7.js"
+  }
+]);
